457d554a655fa83f63eb55359a52cee7ea0e62a6
umair azam <umairazam392@gmail.com>
2022-06-05 21:58:38 +0500

    userregister controller

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Artisan;
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -28,7 +29,7 @@ class RegisteredUserController extends Controller
 
 
 
-  public function __construct()
+    public function __construct()
     {
         $this->middleware('guest');
     }
@@ -50,41 +51,46 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         //ReCpatcha
-        if(env('RECAPTCHA_MODULE') == 'yes')
-        {
+        if (env('RECAPTCHA_MODULE') == 'yes') {
             $validation['g-recaptcha-response'] = 'required|captcha';
-        }else{
+        } else {
             $validation = [];
         }
         $this->validate($request, $validation);
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => ['required', 'string',
-                         'min:8','confirmed', Rules\Password::defaults()],
+            'password' => [
+                'required', 'string',
+                'min:8', 'confirmed', Rules\Password::defaults()
+            ],
         ]);
-
-
-
         $user = User::create([
-            
+
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-             'type' => 'company',
-             'default_pipeline' => 1,
-              'plan' => 1,
-              'lang' => Utility::getValByName('default_language'),
-               'avatar' => '',
-               'created_by' => 1,
+            'type' => 'company',
+            'default_pipeline' => 1,
+            'plan' => 1,
+            'lang' => Utility::getValByName('default_language'),
+            'avatar' => '',
+            'created_by' => 1,
+            'db_name' =>'olaaccounts_' .  $request->name,
+            'db_password' => Hash::make($request->password),
+            'db_username' => 'root',
         ]);
+
+        
+        $new_db_name = 'olaaccounts_' . $request->name;
+        $new_mysql_username = "root";
+        $new_mysql_password = "";
+
         $database = DatabaseInfo::create([
             'user_id' => 1,
-            'db_name' => $request->name,
-            'db_password' => Hash::make($request->password),
+            'db_name' =>$new_db_name,
+            'db_password' => $new_mysql_password,
         ]);
-        
-       
 
         $role_r = Role::findByName('company');
         $user->assignRole($role_r);
@@ -97,53 +103,69 @@ class RegisteredUserController extends Controller
         Utility::jobStage($user->id);
 
         event(new Registered($user));
-        
-        try{
-            $new_db_name = 'olaaccounts_'.$request->name;
-            $new_mysql_username = "root";
-            $new_mysql_password = "";
-          
-            
+
+        try {
+
             $conn = mysqli_connect(
-                config('database.connections.mysql.host'), 
-                env('DB_USERNAME'), 
-                env('DB_PASSWORD')
+                config('database.connections.mysql.host'),
+                'root',
+                env('DB_PASSWORD'),
             );
-            if(!$conn ) {
-                return false;
+            if (!$conn) {
+                return 'false';
             }
-            $sql = 'CREATE Database IF NOT EXISTS '.$new_db_name;
-            $exec_query = mysqli_query( $conn, $sql);
-            if(!$exec_query) {
+            $sql = 'CREATE Database IF NOT EXISTS ' . $new_db_name;
+            $exec_query = mysqli_query($conn, $sql);
+            if (!$exec_query) {
                 die('Could not create database: ' . mysqli_error($conn));
-                
             }
-            $bool= DBConnection($new_db_name);
-         
 
-            $artisan    =   Artisan::call('migrate', array('--database' => $new_db_name,'--path' => 'database/migrations', '--force' => true));
-            if($artisan){
-                return 'Database created successfully with name '.$new_db_name;
+            $conn1 = mysqli_connect(
+                config('database.connections.mysql.host'),
+                $new_mysql_username,
+                $new_mysql_password,
+                $new_db_name,
+            );
 
-            }else{
-                return 'error while artisan command ';   
-            }               
+        if ($conn1) {
+            // return 'done';
+
+            config(["database.connections.myConnection" => [
+                // fill with dynamic data:
+                "driver" => "mysql",
+                "host" =>  env('DB_HOST', '127.0.0.1'),
+                "port" => "DB_PORT', '3306",
+                "database" => $new_db_name, //Here you need to set value of session variable
+                "username" => $new_mysql_username, // credential will be the same for all
+                "password" => $new_mysql_password, // credential will be the same for all
+                "charset" => "utf8",
+                "collation" => "utf8_unicode_ci",
+                "prefix" => "",
+                "strict" => true,
+                "engine" => null
+            ]]);
+
+            $artisan = Artisan::call('migrate', ['--database' => 'myConnection', '--path' => 'database/migrations', '--force' => true]);
+            // $artisan    =   Artisan::call('migrate', array('--database' => $new_db_name,'--path' => 'database/migrations', '--force' => true));
+            if ($artisan) {
+                return 'Database created successfully with name ' . $new_db_name;
+            } else {
+                return 'error while artisan command ';
+            }
         }
-        
-        catch(\Exception $e){
-            return 'Error '.$e->getLine()." ".$e->getMessage(); 
+        } catch (\Exception $e) {
+            return 'Error ' . $e->getLine() . " " . $e->getMessage();
             return false;
-        } 
+        }
 
         Auth::login($user);
 
         return redirect(RouteServiceProvider::HOME);
     }
 
- public function showRegistrationForm($lang = '')
+    public function showRegistrationForm($lang = '')
     {
-        if($lang == '')
-        {
+        if ($lang == '') {
             $lang = Utility::getValByName('default_language');
         }
 
@@ -152,5 +174,4 @@ class RegisteredUserController extends Controller
 
         return view('auth.register', compact('lang'));
     }
-
 }
